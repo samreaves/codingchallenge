3abbe4121cf6623d3c5d1a261082460af3461afd
Sam Reaves <thesamreaves@gmail.com>
2014-12-04 00:24:50 -0500

    Added services and ngTouch to app.js

--- app/app.js
+++ app/app.js
@@ -3,9 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'ngTouch',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.services'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
